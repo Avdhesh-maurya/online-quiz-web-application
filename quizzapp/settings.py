bd21ae15b47cf4ccca2a1432069aa0033c57487c
Avdhesh maurya <akah8887@gmail.com>
2025-09-19 18:31:37 +0530

    Fix STATIC_ROOT configuration for deployment

--- quizzapp/settings.py
+++ quizzapp/settings.py
@@ -170,7 +170,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mainapp/static'),
+]
+
+# WhiteNoise static files configuration
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
